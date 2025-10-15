97a6eeaf8a444860becc9494ed172019533a6deb
efa1lly <endfail.bs@gmail.com>
2025-10-15 22:22:23 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,19 @@
+import numpy
+
+
+rows = int(input("Введите количество строк: "))
+cols = int(input("Введите количество столбцов: "))
+
+
+print("Введите элементы матрицы a:")
+A = numpy.array([list(map(int, input(f"Строка {i+1}: ").split())) for i in range(rows)])
+
+
+print("Введите элементы матрицы b:")
+B = numpy.array([list(map(int, input(f"Строка {i+1}: ").split())) for i in range(rows)])
+
+
+print("Матрица A:")
+print(A)
+print("Матрица B:")
+print(B)
